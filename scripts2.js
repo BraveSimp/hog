2162177c9e00efbe5d24a8980ab5ed46ab4d836c
BraveSimp <76597257+BraveSimp@users.noreply.github.com>
2021-02-03 09:59:38 +0200

    Update scripts2.js

--- scripts2.js
+++ scripts2.js
@@ -1,12 +1,32 @@
-// dont look at this pls
-// DocumentObjectModule Variables ( connection variables)
+// ↓ dont look at that pls ↓
+//  DocumentObjectModule Variables ( connection variables)
 const packagesDeliveredP = document.querySelector("#packages-delivered-p");
 const packagesDeliveredBtn = document.querySelector("#packages-delivered-btn");
-// Game Variables ( variables used in the game duh)
+const firstHogBtn = document.querySelector("#first-hog-btn");
+// ↓ Game Variables ( variables used in the game duh) ↓
 let packagesDelivered = 0;
 
+// Functions
+const autoPackageDeliver = () => {
+  packagesDelivered += 1;
+};
+const updateUI = () => {
+  packagesDeliveredP.innerHTML = `Packages delivered: ${cookieNum}`;
+};
 // Event listener
 packagesDeliveredBtn.addEventListener("click", () => {
   packagesDelivered += 1;
   packagesDeliveredP.innerHTML = `Packages delivered: ${packagesDelivered}`;
+  updateUI();
 });
+
+firstHogBtn.addEventListener("click", () => {
+  packagesDelivered += 3;
+  packagesDeliveredP.innerHTML = `Packages delivered: ${packagesDelivered}`;
+  updateUI();
+});
+
+// Game Loop
+window.setInterval(() => {
+  updateUI();
+}, 1000);
