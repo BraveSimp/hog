b84ce9216e4840528e56e71be9f93c09809ffe7e
BraveSimp <76597257+BraveSimp@users.noreply.github.com>
2021-02-19 23:49:48 +0200

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,13 +1,18 @@
 // ↓ dont look at that pls ↓
 //  DocumentObjectModel Variables ( connection variables)
 const packagesDeliveredP = document.querySelector("#packages-delivered-p");
-const packagesDeliveredBtn = document.querySelector("#packages-delivered-btn");
-const hogCounters = document.querySelector("#hog-counters"); //hog-buttons
+const packagesDeliveredBtn = document.querySelector("#deliver-package-btn");
 const hogButtons = document.querySelector("#hog-buttons");
+const savingButton = document.querySelector("#saving-button");
+// beautyfunction
+function beautify(num) {
+  return num > 100000 ? num.toExponential(2) : num;
+}
 
 // ↓ Game Variables ( variables used in the game duh) ↓
 let packagesDelivered = 0;
 
+// saving stuffs
 const saveGame = () => {
   const gameSave = {
     packagesDelivered,
@@ -17,8 +22,6 @@ const saveGame = () => {
 };
 function loadSave() {
   var saveData = localStorage.getItem("Hogs of Gods");
-  // Implementation when
-  //printMessageLine("Loaded.");
   console.log(saveData);
   if (!saveData) return;
   console.log(saveData);
@@ -28,6 +31,11 @@ function loadSave() {
   for (const hogId in gameSave.hogTypes)
     hogTypes[hogId].amount = gameSave.hogTypes[hogId].amount;
 }
+
+savingButton.addEventListener("click", () => {
+  saveGame();
+});
+
 // hog type OOP shit
 const hogTypes = [];
 
@@ -48,14 +56,11 @@ class HogType {
     this.buyButton.classList.add("hog-button");
     this.buyButton.addEventListener("click", () => this.buy());
     hogButtons.appendChild(this.buyButton);
-    this.hogCounter = document.createElement("p");
-    this.hogCounter.classList.add("hog-counter");
-    hogCounters.appendChild(this.hogCounter);
     hogTypes.push(this);
   }
   updateUI() {
-    this.buyButton.innerText = `${this.name}: ${this.cost}`;
-    this.hogCounter.innerText = `${this.name}s bought: ${this.amount}`;
+    this.buyButton.innerText = `${this.name}: ${beautify(this.cost)}
+    Amount bought: ${beautify(this.amount)}`;
   }
   buy() {
     if (packagesDelivered < this.cost) return;
@@ -74,9 +79,15 @@ const autoPackageDeliver = () => {
   for (const hogType of hogTypes) hogType.applyGains();
 };
 const updateUI = () => {
-  packagesDeliveredP.innerHTML = `Packages delivered: ${packagesDelivered}`;
+  packagesDeliveredP.innerHTML = `Packages delivered: ${beautify(
+    packagesDelivered
+  )}`;
   for (const hogType of hogTypes) hogType.updateUI();
 };
+//const autoPackagesPerSecond = () => {
+//packagesPerSecond = "First Hog";
+//};
+
 // Event listener
 packagesDeliveredBtn.addEventListener("click", () => {
   packagesDelivered += 1;
@@ -90,15 +101,14 @@ new HogType("Fifth Hog");
 new HogType("Sixth Hog");
 new HogType("Seventh Hog");
 new HogType("Eighth Hog");
-new HogType("1 H0G 2 rul dem aLLL!!!!!!");
-
+new HogType("1 H0G 2 rul dem aLL!1!");
 // Game Loop
 window.setInterval(() => {
   autoPackageDeliver();
 }, 1000);
 window.setInterval(() => {
   saveGame();
-}, 30000);
+}, 15000);
 
 window.setInterval(() => {
   updateUI();
